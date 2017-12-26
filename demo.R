446e33bb595db708c1cac5c6431c06e40231e763
Liu Lab <gordonliu810822@gmail.com>
2017-12-26 15:58:42 +0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -7,18 +7,20 @@ n = 200; p = 1000; K = 10; ratio = 1; sigma=0.5; kind = 1; seednum = NULL; rho =
 corr = NULL; snr = 2;
 dat = gendata(n,p,K,sigma,ratio,seednum,kind,rho,corr,snr)
 X = dat$Xp; y = dat$y;  D = dat$D; ye = dat$ye; A = dat$A; xe = dat$xe;
-
-out=BIC.PDAS(X, y, 'lasso', 'vote', 0, tau= 2.7, 200, 0, 1, 1, 0.01, 100);
+method = "lasso";
+sel="bic"; alpha = 0; mu = n/log(n); del = 0; MaxIt = 1; Lmax = 1; tau = 0; Lmin = 0.01; Nstep = 100;
+#alpha  ---  regularization parameter (default: 0)                #
+#method ---  nonconvex model                                      #
+#tau    ---  concavity parameter                                  #
+#mu     ---  stopping parameter  (default: n/log(n)               #
+#MaxIt  ---  maximum number of iterations (default: 1)            #
+#del    --- noise level (default: 0)                              #
+#sel    --- tunning parameter selection method (defaut:vote)      #
+#Lmin   --- minimun in Lam (defaut: 1e-3)                         #
+#N      --- length of path (defaut: 100)                          #
+out=BIC.PDAS(X, y, method, sel, alpha, tau, mu, del,MaxIt, Lmax, Lmin, Nstep);
 out2=list(ithist_x=matrix(rep(1/D,dim(out$ithist_x)[2]),ncol=dim(out$ithist_x)[2])*
 out$ithist_x,ithist_as = out$ithist_as, ithist_bic = out$ithist_bic);
 plot.PDAS(out2,xvar="Index");
 bic = out2$ithist_bic;
 bh = out2$ithist_x[,which(bic==min(bic))];
-
-
-#library(glmnet)
-#fit = glmnet(X, y,lambda.min.ratio=0.01)
-#fit$beta=(1/D)*fit$beta
-#plot(fit)
-#cv = cv.glmnet(X,y,lambda.min.ratio=0.01)
-#plot(cv)
